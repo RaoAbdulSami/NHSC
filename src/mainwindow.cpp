5f77ffc673a1d5262221c82f162939071252113e
Abdul Sami <abdul.sami@rwrlimited.com>
2023-12-20 16:28:31 +0500

    feat: app created , cmake need to be updated

---
+++ src/mainwindow.cpp
@@ -0,0 +1,55 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    //ui->textEdit->setLineWrapMode();
+}
+
+void MainWindow::on_CalcSumBtn_clicked()
+{
+    QString hexValue = ui->HexInputBox->toPlainText();
+
+    int sum = 0x00;
+
+
+    QString hexPair;
+
+    if(hexValue.length()%2 ==0)
+    {
+
+        for (int i = 0; i < hexValue.length(); i += 2)
+        {
+            hexPair = hexValue.mid(i, 2);
+            bool ok = false;
+            int value = hexPair.toInt(&ok, 16);
+            if (ok)
+            {
+                sum += value;
+            }
+        }
+
+
+        ui->ShowHexResultBox->setText(QString::number(sum,16));
+        ui->ShowDecimalResultBox->setText(QString::number(sum,10));
+        ui->InputTextLengthLabel->setText("Bytes entered : " + QString::number(hexValue.length()/2));
+    }
+
+    else
+    {
+        ui->InputTextLengthLabel->setText("Please Enter values in even pairs " );
+
+    }
+
+}
+
